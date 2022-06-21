1b76f749c175b41e71b9fe6699a14ea57c5363fd
Javi <rtomyj@gmail.com>
2022-06-20 21:08:16 -0500

    mysql connect feature

--- main.go
+++ main.go
@@ -6,9 +6,18 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
+	// ApiCall()
+	SkcDBConn()
+}
+
+func ApiCall() {
 	res, err := http.Get(SkcBaseUrl + CardInfoEndpoint)
 	if err != nil {
 		log.Fatalln("There was an error fetching info: ", err)
@@ -23,4 +32,37 @@ func main() {
 	fmt.Println("Name of card: ", cardInfoResponse.CardID)
 	fmt.Println("Name of card: ", cardInfoResponse.CardAttribute)
 	fmt.Println("Name of card: ", cardInfoResponse.CardEffect)
+}
+
+type Card struct {
+	CardName string `db:"card_name"`
+}
+
+func SkcDBConn() {
+	db, err := sql.Open("mysql", "root@/skc_api_db")
+
+	if err != nil {
+		log.Fatalln("Error occurred while trying to establish DB connection", err)
+	}
+
+	rows, err := db.Query("select card_name from cards")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var card Card
+		err := rows.Scan(&card.CardName)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Printf("%v\n", card)
+	}
+
+	defer db.Close()
 }
