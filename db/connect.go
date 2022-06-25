e6785ea9ec7f4c36251393c532f1b8b04aa61fc5
Javi <rtomyj@gmail.com>
2022-06-25 18:31:00 -0500

    Hint

--- db/connect.go
+++ db/connect.go
@@ -12,7 +12,7 @@ var (
 // Connect to SKC database.
 func EstablishSKCDBConn() {
 	var err error
-	skcDBConn, err = sql.Open("mysql", "root@/skc_api_db")
+	skcDBConn, err = sql.Open("mysql", "root@/skc_api_db") // root:PWD@tcp(skc-api-db:3306)/skc_api_db for docker, TODO: use env vars to set this dynamically
 
 	if err != nil {
 		log.Fatalln("Error occurred while trying to establish DB connection: ", err)
