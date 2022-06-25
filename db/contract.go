53a615f8b0080c1c4de8d82de3f15317cf20845c
Javi <rtomyj@gmail.com>
2022-06-25 00:35:45 -0500

    Added more fileds for struct

--- db/contract.go
+++ db/contract.go
@@ -1,7 +1,12 @@
 package db
 
 type Card struct {
-	CardID     string `db:"card_number" json:"cardID"`
-	CardName   string `db:"card_name" json:"cardName"`
-	CardEffect string `db:"card_effect" json:"cardEffect"`
+	CardID         string `db:"card_number" json:"cardID"`
+	CardColor      string `db:"card_color" json:"cardColor"`
+	CardName       string `db:"card_name" json:"cardName"`
+	CardAttribute  string `db:"card_attribute" json:"cardAttribute"`
+	CardEffect     string `db:"card_effect" json:"cardEffect"`
+	MonsterType    string `db:"monster_type" json:"monsterType"`
+	MonsterAttack  int32  `db:"monster_attack" json:"monsterAttack"`
+	MonsterDefense int32  `db:"monster_defense" json:"monsterDefense"`
 }
