b2176d3d56e2915ef705eaf43b4fbe15989f5dac
Javi <rtomyj@gmail.com>
2022-06-30 19:39:43 -0500

    Cleanup

--- db/connect.go
+++ db/connect.go
@@ -16,7 +16,7 @@ func EstablishSKCDBConn() {
 	dataSourceName := env.EnvMap["SKC_DB_USER"] + ":" + env.EnvMap["SKC_DB_PWD"] + "@tcp(" + env.EnvMap["SKC_DB_URI"] + ")/" + env.EnvMap["SKC_DB_NAME"]
 
 	var err error
-	if skcDBConn, err = sql.Open("mysql", dataSourceName); err != nil { // root:PWD@tcp(skc-api-db:3306)/skc_api_db for docker, TODO: use env vars to set this dynamically
+	if skcDBConn, err = sql.Open("mysql", dataSourceName); err != nil {
 		log.Fatalln("Error occurred while trying to establish DB connection: ", err)
 	}
 }
