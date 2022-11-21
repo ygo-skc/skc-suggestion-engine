275f6b1b5abfce527b46d13515f9ef8e2c136b02
Javi <rtomyj@gmail.com>
2022-11-21 12:39:38 -0600

    Securing api with ssl

--- api/api.go
+++ api/api.go
@@ -71,7 +71,7 @@ func SetupMultiplexer() {
 	})
 
 	log.Println("Starting server in port 9000")
-	if err := http.ListenAndServe(":9000", corsOpts.Handler(router)); err != nil { // docker does not like localhost:9000 so im just using port number
+	if err := http.ListenAndServeTLS(":9000", "certs/certificate.crt", "certs/private.key", corsOpts.Handler(router)); err != nil { // docker does not like localhost:9000 so im just using port number
 		log.Fatalln("There was an error starting api server: ", err)
 	}
 }
--- doppler-secrets-local-setup.sh
+++ doppler-secrets-local-setup.sh
@@ -8,3 +8,7 @@ doppler secrets download -p skc-suggestion-engine -c prod --no-file --format env
 # Download Certs
 mkdir certs
 doppler secrets get -p skc-suggestion-engine -c prod MONGODB_X509 --plain  > certs/skc-suggestion-engine-db.pem
+
+doppler secrets get -p skc-suggestion-engine -c prod SSL_CERTIFICATE --plain  > certs/certificate.crt
+doppler secrets get -p skc-suggestion-engine -c prod SSL_PRIVATE_KEY --plain  > certs/private.key
+doppler secrets get -p skc-suggestion-engine -c prod SSL_CA_BUNDLE --plain  > certs/ca_bundle.crt
