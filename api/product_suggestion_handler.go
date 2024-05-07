b72b6d07089d6500432b3d9365de3525f16b9ba3
Javi <8519394+rtomyj@users.noreply.github.com>
2024-05-06 23:57:35 -0500

    Using path param instead of hard coded value

--- api/product_suggestion_handler.go
+++ api/product_suggestion_handler.go
@@ -2,14 +2,20 @@ package api
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func getProductSuggestionsHandler(res http.ResponseWriter, req *http.Request) {
-	productID := "LEDE"
-	x, _ := skcDBInterface.GetCardsFoundInProduct(productID)
+	pathVars := mux.Vars(req)
+	productID := pathVars["productID"]
+	log.Printf("Getting card suggestions for product w/ ID: %s", productID)
 
+	x, _ := skcDBInterface.GetCardsFoundInProduct(productID)
 	ccIds, _ := skcDBInterface.GetCardColorIDs() // retrieve card color IDs
+
 	suggestions := getBatchSuggestions(&x.CardInfo, make([]string, 0), ccIds)
 
 	res.WriteHeader(http.StatusOK)
