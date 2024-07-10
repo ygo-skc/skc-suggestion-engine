73d3ab227104e55bc43bf92ac9a74a192192df31
Javi <8519394+rtomyj@users.noreply.github.com>
2024-07-10 16:09:56 -0500

    Made suggestion and support calls async

--- api/product_suggestion_handler.go
+++ api/product_suggestion_handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/mux"
 	"github.com/ygo-skc/skc-suggestion-engine/model"
@@ -18,7 +19,7 @@ func getProductSuggestionsHandler(res http.ResponseWriter, req *http.Request) {
 	logger, ctx := util.NewRequestSetup(context.Background(), "product card suggestions", slog.String("productID", productID))
 	logger.Info("Getting product card suggestions")
 
-	cardsInProductChan, ccIDsChan := make(chan model.BatchCardData[model.CardIDs]), make(chan map[string]int)
+	cardsInProductChan, ccIDsChan := make(chan model.BatchCardData[model.CardIDs], 1), make(chan map[string]int, 1)
 	go func() {
 		cardsInProduct, _ := skcDBInterface.GetCardsFoundInProduct(ctx, productID)
 		cardsInProduct.UnknownResources = make(model.CardIDs, 0) // by default, no unknown ids
@@ -28,10 +29,16 @@ func getProductSuggestionsHandler(res http.ResponseWriter, req *http.Request) {
 		ccIDs, _ := skcDBInterface.GetCardColorIDs(ctx) // retrieve card color IDs
 		ccIDsChan <- ccIDs
 	}()
-
 	cardsInProduct := <-cardsInProductChan
-	suggestions := getBatchSuggestions(ctx, cardsInProduct, <-ccIDsChan)
-	support := getBatchSupport(ctx, cardsInProduct)
+
+	var suggestions model.BatchCardSuggestions[model.CardIDs]
+	var support model.BatchCardSupport[model.CardIDs]
+	var wg sync.WaitGroup
+
+	wg.Add(2)
+	go func() { defer wg.Done(); suggestions = getBatchSuggestions(ctx, cardsInProduct, <-ccIDsChan) }()
+	go func() { defer wg.Done(); support = getBatchSupport(ctx, cardsInProduct) }()
+	wg.Wait()
 
 	res.WriteHeader(http.StatusOK)
 	json.NewEncoder(res).Encode(model.ProductSuggestions[model.CardIDs]{Suggestions: suggestions, Support: support})
