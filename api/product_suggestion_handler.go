4a69b80af68e36131af8ac3b9d3397c9f89b5dc8
Javi <8519394+rtomyj@users.noreply.github.com>
2025-01-01 16:13:48 -0600

    Simplifying logic

--- api/product_suggestion_handler.go
+++ api/product_suggestion_handler.go
@@ -19,24 +19,22 @@ func getProductSuggestionsHandler(res http.ResponseWriter, req *http.Request) {
 	logger, ctx := util.NewRequestSetup(context.Background(), "product card suggestions", slog.String("productID", productID))
 	logger.Info("Getting product card suggestions")
 
-	cardsInProductChan, ccIDsChan := make(chan model.BatchCardData[model.CardIDs]), make(chan map[string]int)
+	cardsInProductChan := make(chan model.BatchCardData[model.CardIDs])
 	go func() {
 		cardsInProduct, _ := skcDBInterface.GetCardsFoundInProduct(ctx, productID)
 		cardsInProduct.UnknownResources = make(model.CardIDs, 0) // by default, no unknown ids
 		cardsInProductChan <- cardsInProduct
 	}()
-	go func() {
-		ccIDs, _ := skcDBInterface.GetCardColorIDs(ctx) // retrieve card color IDs
-		ccIDsChan <- ccIDs
-	}()
-	cardsInProduct := <-cardsInProductChan
+
+	ccIDs, _ := skcDBInterface.GetCardColorIDs(ctx) // retrieve card color IDs
 
 	var suggestions model.BatchCardSuggestions[model.CardIDs]
 	var support model.BatchCardSupport[model.CardIDs]
 	var wg sync.WaitGroup
 
 	wg.Add(2)
-	go func() { defer wg.Done(); suggestions = getBatchSuggestions(ctx, cardsInProduct, <-ccIDsChan) }()
+	cardsInProduct := <-cardsInProductChan
+	go func() { defer wg.Done(); suggestions = getBatchSuggestions(ctx, cardsInProduct, ccIDs) }()
 	go func() { defer wg.Done(); support = getBatchSupport(ctx, cardsInProduct) }()
 	wg.Wait()
 
