82ca0b95292ba5fb38567ad9e3da753586675a7c
Javi <8519394+rtomyj@users.noreply.github.com>
2025-01-23 14:17:26 -0600

    Excluding file as it will be generated on API start

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,6 @@
-if [ $# -eq 0 ]
-	then
-		echo "Need server name"
-		exit 1
+if [ $# -eq 0 ]; then
+	echo "Need server name"
+	exit 1
 fi
 
 SERVER=$1
@@ -14,10 +13,10 @@ env GOOS=linux GOARCH=arm64 go build .
 echo "Using server $SERVER and directory $DIR_ON_SERVER to sync prod API"
 
 echo "Uploading API files"
-rsync --rsync-path="mkdir -p ${DIR_ON_SERVER} && rsync" -avzh --delete --progress -e "ssh -i ~/.ssh/skc-server.pem" skc-suggestion-engine data certs .env_prod docker-compose.yaml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
+rsync --rsync-path="mkdir -p ${DIR_ON_SERVER} && rsync" -avzh --delete --progress --exclude="certs/concatenated.crt" -e "ssh -i ~/.ssh/skc-server.pem" skc-suggestion-engine data certs .env_prod docker-compose.yaml "${USER}@${SERVER}:${DIR_ON_SERVER}/"
 
 echo -e "\n\nRestaging API"
-ssh -i ~/.ssh/skc-server.pem "${USER}@${SERVER}" << EOF
+ssh -i ~/.ssh/skc-server.pem "${USER}@${SERVER}" <<EOF
 	cd $DIR_ON_SERVER
 	docker-compose kill
 	docker-compose rm -f
