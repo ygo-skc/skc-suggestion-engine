a74246a0f7fe127aa2eb28a93baca329c8aa4065
Javi <8519394+rtomyj@users.noreply.github.com>
2026-07-10 16:48:22 -0500

    Making status calls in parallel

--- api/status_handler.go
+++ api/status_handler.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 
 	cModel "github.com/ygo-skc/skc-go/common/v2/model"
 	cUtil "github.com/ygo-skc/skc-go/common/v2/util"
@@ -20,26 +21,37 @@ const (
 func getAPIStatusHandler(res http.ResponseWriter, req *http.Request) {
 	logger, ctx := cUtil.InitRequest(context.Background(), apiName, statusOp)
 
-	downstreamHealth := []cModel.DownstreamItem{}
+	downstreamHealth := make([]cModel.DownstreamItem, 2)
 
 	var ygoServiceVersion string
 	var skcSuggestionDBVersion string
 
+	var wg sync.WaitGroup
+	wg.Add(2)
+
 	// get status on SKC DB by checking the version number. If this operation fails, its save to assume the DB is down.
-	if ygoServiceStatus, err := downstream.YGO.HealthService.GetAPIStatus(ctx); err != nil {
-		downstreamHealth = append(downstreamHealth, cModel.DownstreamItem{ServiceName: "YGO Service", Status: cModel.Down})
-	} else {
-		downstreamHealth = append(downstreamHealth, cModel.DownstreamItem{ServiceName: "YGO Service", Status: cModel.Up, Version: ygoServiceStatus.Version})
-		ygoServiceVersion = ygoServiceStatus.Version
-	}
+	go func() {
+		defer wg.Done()
+		if ygoServiceStatus, err := downstream.YGO.HealthService.GetAPIStatus(ctx); err != nil {
+			downstreamHealth[0] = cModel.DownstreamItem{ServiceName: "YGO Service", Status: cModel.Down}
+		} else {
+			downstreamHealth[0] = cModel.DownstreamItem{ServiceName: "YGO Service", Status: cModel.Up, Version: ygoServiceStatus.Version}
+			ygoServiceVersion = ygoServiceStatus.Version
+		}
+	}()
 
 	// get status on SKC Suggestion DB by checking the version number. If this operation fails, its save to assume the DB is down.
-	if dbVersion, err := skcSuggestionEngineDBInterface.GetSKCSuggestionDBVersion(ctx); err != nil {
-		downstreamHealth = append(downstreamHealth, cModel.DownstreamItem{ServiceName: "SKC Suggestion Engine DB", Status: cModel.Down})
-	} else {
-		downstreamHealth = append(downstreamHealth, cModel.DownstreamItem{ServiceName: "SKC Suggestion Engine DB", Status: cModel.Up})
-		skcSuggestionDBVersion = dbVersion
-	}
+	go func() {
+		defer wg.Done()
+		if dbVersion, err := skcSuggestionEngineDBInterface.GetSKCSuggestionDBVersion(ctx); err != nil {
+			downstreamHealth[1] = cModel.DownstreamItem{ServiceName: "SKC Suggestion Engine DB", Status: cModel.Down}
+		} else {
+			downstreamHealth[1] = cModel.DownstreamItem{ServiceName: "SKC Suggestion Engine DB", Status: cModel.Up}
+			skcSuggestionDBVersion = dbVersion
+		}
+	}()
+
+	wg.Wait()
 
 	status := cModel.APIHealth{Version: "2.2.5", Downstream: downstreamHealth}
 
